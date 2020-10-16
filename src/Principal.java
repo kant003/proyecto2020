efdb60f7b4b5d4a2c1b7eed070e6dcb20e6377c2
drak alexys <aitorpenedo97@gmail.com>
2020-10-16 14:02:10 +0200

    DNI

--- src/Principal.java
+++ src/Principal.java
@@ -28,6 +28,57 @@ public class Principal {
 	public Principal() {
 		initialize();
 	}
+	
+	class ValidadorDNI
+	{
+		private String dni;
+
+			public ValidadorDNI(String dni) {
+				this.dni = dni;
+			}
+
+		
+		public boolean validar() {	
+			
+			if(dni.length() != 9 || Character.isLetter(dni.charAt(8)) == false ) {
+				return false;
+			}
+
+			
+			if(soloNumeros() == true) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+
+			private boolean soloNumeros() {
+
+				int i, j = 0;
+				String numero = ""; 
+				String miDNI = "";
+				String[] unoNueve = {"0","1","2","3","4","5","6","7","8","9"};
+
+				for(i = 0; i < this.dni.length() - 1; i++) {
+					numero = this.dni.substring(i, i+1);
+
+					for(j = 0; j < unoNueve.length; j++) {
+						if(numero.equals(unoNueve[j])) {
+							miDNI += unoNueve[j];
+						}
+					}
+				}
+
+				if(miDNI.length() != 8) {
+					return false;
+				}
+				else {
+					return true;
+				}
+			}
+		
+	}
 
 	/**
 	 * Initialize the contents of the frame.
