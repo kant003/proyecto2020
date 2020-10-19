ba2567a379133d3d1c63ee6044575528c376d490
adriip13 <adrian.paz.97@gmail.com>
2020-10-19 09:07:16 +0200

    subo como sacar la primera letra

--- src/TextUtility.java
+++ src/TextUtility.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
 
 public class TextUtility {
 
-	
-	
-	
+	public static void main(String[] args) {
+		Scanner sca = new Scanner(System.in);
+		System.out.println("Introduzca el texto:");
+		String word1 = sca.nextLine();		
+		System.out.println(word1.charAt(0));
+		sca.close();
+	}
 }
