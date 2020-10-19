8ccf585641c90ed4faa91cc70140b285d30ba94d
unknown <pablosobral17@gmail.com>
2020-10-19 13:00:41 +0200

    subo uh3

--- src/TextUtility.java
+++ src/TextUtility.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
 
 public class TextUtility {
-
+	static Scanner paco = new Scanner(System.in);
 	
+	public static String TextoMayusuculas(String texto) {
+		
+		texto=paco.next();
+		texto.toUpperCase();
+		
+		return texto;
+	}
 	
 	
 }
