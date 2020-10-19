3aa3918fea53b4a8afcf79a95924243b52272d2f
Simoes-Miguel <msimoescostas@gmail.com>
2020-10-19 13:18:13 +0200

    Subo uh8, historial de palabras

--- src/TextUtility.java
+++ src/TextUtility.java
@@ -1,7 +1,22 @@
+import java.io.*;
 
 public class TextUtility {
+	
 
-	
-	
+	/**
+	 * Funcion que recibe un texto y este se guarda en el fichero "historial.txt"
+	 * los textos guardados estan separados por un triple espacio en el fichero
+	 * 
+	 * @param textoRecibido String a guardar en el historial
+	 * @throws IOException
+	 */
+	public void guardarEnHistorial(String textoRecibido) throws IOException {
+		File historial = new File("historial.txt");
+		FileWriter fw = null;
+
+		fw = new FileWriter(historial, true);
+		fw.write("   " + textoRecibido);
+		fw.close();
+	}
 	
 }
