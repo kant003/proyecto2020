c3c9295973f7d97a2c463c4ec93698c37412307d
mspablo <pablosobral17@gmail>
2020-10-26 12:45:06 +0100

    Subo mayusculas

--- src/TextUtility.java
+++ src/TextUtility.java
@@ -2,24 +2,8 @@ import java.io.*;
 
 public class TextUtility {
 		
-	public static String TextoEnMinúsculas(String texto) {
+	public static String TextoEnMayusculas(String texto) {
 		return texto.toUpperCase();
 	}
 	
-	/**
-	 * Funcion que recibe un texto y este se guarda en el fichero "historial.txt"
-	 * los textos guardados estan separados por un triple espacio en el fichero
-	 * 
-	 * @param textoRecibido String a guardar en el historial
-	 * @throws IOException
-	 */
-	public void guardarEnHistorial(String textoRecibido) throws IOException {
-		File historial = new File("historial.txt");
-		FileWriter fw = null;
-
-		fw = new FileWriter(historial, true);
-		fw.write("   " + textoRecibido);
-		fw.close();
-	}
-
 }
